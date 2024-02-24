d25926711968d046a1a086caf002fe8a49223dfd
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-24 14:11:29 -0500

    completed model classes for vending machine and slot

--- VendingMachine/Models/Slot.cs
+++ VendingMachine/Models/Slot.cs
@@ -1,9 +1,13 @@
-﻿namespace VendingMachine.Models
+﻿namespace VendingMachine.Models;
+
+internal sealed class Slot
 {
-    internal class Slot
-    {
-        internal required string Identifier { get; set; }
-        internal Stack<Snack> Snacks { get; set; } = new Stack<Snack>();
-        internal bool IsSoldOut => Snacks.Count == 0;
-    }
+    internal const string SNACK_SOLD_OUT = "Snack sold out.";
+
+    internal required string Identifier { get; set; }
+    internal Stack<Snack> Snacks { get; set; } = new Stack<Snack>();
+    internal bool IsSoldOut => Snacks.Count == 0;
+
+    public override string ToString() =>
+        string.Format("{0,-5}{1}", Identifier, Snacks.Count > 0 ? Snacks.First().ToString() : SNACK_SOLD_OUT);
 }
---
+++ VendingMachine/Models/Vendomatic.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using VendingMachine.Services;
+
+namespace VendingMachine.Models;
+
+internal sealed class Vendomatic
+{
+    #region constants
+    private const string NO_MONEY_DEPOSITED = "Please deposit money before purchasing snack.";
+    private const string NO_CHANGE_TO_DISPENSE = "No change to dispense.";
+    private const string ALL_SNACKS_SOLD_OUT = "All snacks sold out!";        
+    private const string VENDING_MACHINE_MANUFACTURER = "Umbrella Corp";
+    private const string VENDING_MACHINE_MODEL = "Vendo-Matic 600";
+    public const int SNACKS_PER_SLOT = 5;
+    #endregion
+
+    internal bool IsOn { get; set; }
+    internal IEnumerable<Slot> Slots { get; set; } = Enumerable.Empty<Slot>();
+    internal decimal AmountDeposited { get; set; }
+    internal ICollection<Snack> SnacksSoldToday { get; set; } = Enumerable.Empty<Snack>().ToList();
+    internal StringBuilder AuditFile { get; set; } = new();
+    internal decimal DailySales { get; set; }
+
+    internal bool AreAllSnacksSoldOut => Slots.All(s => s.IsSoldOut);
+    internal bool CustomerHasBalance => AmountDeposited > 0;
+
+    internal string DisplaySnacks()
+    {
+        if (AreAllSnacksSoldOut) { return ALL_SNACKS_SOLD_OUT; }
+
+        StringBuilder sb = new("*** SNACKS ***\n\n");
+
+        foreach (Slot slot in Slots)
+        {
+            sb.AppendLine(slot.ToString());
+        }
+
+        return sb.ToString();
+    }
+    internal string DispenseSnack(Slot slot)
+    {
+        if (AmountDeposited <= 0) { return NO_MONEY_DEPOSITED; }
+        if (slot.IsSoldOut) { return Slot.SNACK_SOLD_OUT; }
+
+        Snack snack = slot.Snacks.Peek();
+        if (snack.Price > AmountDeposited) { return FundsInsufficientForPurchase(snack.Price, snack.Label); }
+
+        snack = slot.Snacks.Pop();
+
+        AmountDeposited -= snack.Price;
+        DailySales += snack.Price;
+
+        SnacksSoldToday.Add(snack);
+        AuditFile.AppendLine(AuditFileLineItem.SnackSold(snack, slot.Identifier, AmountDeposited));
+
+        return snack.SendMessageWhenSold();
+    }
+    internal string FinishTransaction()
+    {
+        if (AmountDeposited <= 0) { return NO_CHANGE_TO_DISPENSE; }
+
+        decimal changeDispensed = AmountDeposited;
+
+        (int Quarters, int Dimes, int Nickels) = Helpers.CountCoinsForChange(AmountDeposited);
+        if (Quarters != -1 && Helpers.ChangeDispensedReducesAmountDepositedToZero((Quarters, Dimes, Nickels), AmountDeposited))
+        {
+            AmountDeposited = 0M;
+            AuditFile.AppendLine(AuditFileLineItem.ChangeDispensed(changeDispensed, AmountDeposited));
+        }
+
+        return FormatChangeOutput(Quarters, Dimes, Nickels);
+
+        static string FormatChangeOutput(int numQuarters, int numDimes, int numNickels)
+        {
+            StringBuilder sb = new();
+            sb.Append("Your change is: ");
+            if (numQuarters > 0)
+            {
+                if (numQuarters == 1) { sb.Append(string.Format("{0} quarter", numQuarters)); }
+                if (numQuarters > 1) { sb.Append(string.Format("{0} quarters", numQuarters)); }
+                if (numDimes > 0 || numNickels > 0) { sb.Append(", "); }
+            }
+            if (numDimes > 0)
+            {
+                if (numDimes == 1) { sb.Append(string.Format("{0} dime", numDimes)); }
+                if (numDimes > 1) { sb.Append(string.Format("{0} dimes", numDimes)); }
+                if (numNickels > 0) { sb.Append(", "); }
+            }
+            if (numNickels > 0)
+            {
+                if (numNickels == 1) { sb.Append(string.Format("{0} nickel", numNickels)); }
+                if (numNickels > 1) { sb.Append(string.Format("{0} nickels", numNickels)); }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    internal string FundsInsufficientForPurchase(decimal price, string label) => $"You have not deposited enough money to purchase {label} \n The price for {label} is {price:C} and you have deposited {AmountDeposited:C}.";
+    internal string DisplayAmountDeposited() => $"Amount deposited: {AmountDeposited:C}";
+    
+}
