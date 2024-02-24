ab1dd4c58a184e786775870bfa932cbb1e35c6fb
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-24 14:10:53 -0500

    completed static customer class

--- VendingMachine/Models/Customer.cs
+++ VendingMachine/Models/Customer.cs
@@ -1,8 +1,13 @@
-﻿namespace VendingMachine.Models
+﻿namespace VendingMachine.Models;
+
+internal static class Customer
 {
-    internal class Customer
+    internal static void DepositMoney(Vendomatic vm, decimal amount)
     {
-        internal void DepositMoney() { throw new NotImplementedException(); }
-        internal void SelectSnack() { throw new NotImplementedException(); }
+        if (amount <= 0) { return; }
+        vm.AmountDeposited += amount;
+        vm.AuditFile.AppendLine(AuditFileLineItem.MoneyDeposited(amount, vm.AmountDeposited));
     }
+
+    internal static string SelectSnack(Vendomatic vm, Slot slot) => vm.DispenseSnack(slot);
 }
