52be6611ac897afdf7d9a6f6622d5edbe6e715c8
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-24 14:10:33 -0500

    added class for audit file line items

---
+++ VendingMachine/Models/AuditFileLineItem.cs
@@ -0,0 +1,13 @@
+﻿using VendingMachine.Services;
+
+namespace VendingMachine.Models;
+
+internal static class AuditFileLineItem
+{
+    internal static string MoneyDeposited(decimal amountDeposited, decimal totalDeposited) =>
+        $"{Helpers.FormatedDateTimeNow}|FEED MONEY|AMOUNT DEPOSITED: {amountDeposited:C}|TOTAL DEPOSITED: {totalDeposited:C}";
+    internal static string SnackSold(Snack snack, string slotIdentifier, decimal totalDeposited) =>
+        $"{Helpers.FormatedDateTimeNow}|SNACK SOLD: {snack.Label}|SLOT: {slotIdentifier}|SNACK PRICE: {snack.Price:C}|TOTAL DEPOSTED: {totalDeposited:C}";
+    internal static string ChangeDispensed(decimal change, decimal totalDeposited) =>
+        $"{Helpers.FormatedDateTimeNow}|CHANGE DISPENSED: {change:C}|TOTAL DEPOSITED: {totalDeposited:C}";        
+}
