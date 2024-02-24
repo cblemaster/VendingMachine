bed5c69bcef67cade09ad9ab1adc774fbbd98527
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-24 14:07:11 -0500

    added user interface

---
+++ VendingMachine/UI/IBasicUserInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace VendingMachine.UI;
+
+public interface IBasicUserInterface
+{
+    void Output(string message);
+    void PauseOutput();
+    object PromptForSelection(object[] options);
+}
