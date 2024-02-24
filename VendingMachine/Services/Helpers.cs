5d12c95903974f1729e8fd8dfad8d895636c48b8
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-24 14:12:29 -0500

    added helper class

---
+++ VendingMachine/Services/Helpers.cs
@@ -0,0 +1,43 @@
+﻿namespace VendingMachine.Services;
+
+internal static class Helpers
+{
+    internal static string FormatedDateTimeNow => DateTime.Now.ToString("M-d-yyyy H:mm:ss");
+
+    internal static (int Quarters, int Dimes, int Nickels) CountCoinsForChange(decimal change)
+    {
+        int numQuarters = CalculateCoinCount(change, Coins.Quarter);
+        change -= numQuarters * (((int)Coins.Quarter) / 100M);
+
+        int numDimes = CalculateCoinCount(change, Coins.Dime);
+        change -= numDimes * (((int)Coins.Dime) / 100M);
+
+        int numNickels = CalculateCoinCount(change, Coins.Nickel);
+        change -= numNickels * (((int)Coins.Nickel) / 100M);
+
+        return change == 0 ? (numQuarters, numDimes, numNickels) : (-1, -1, -1);
+
+        static int CalculateCoinCount(decimal amount, Coins coin)
+        {
+            return (int)(amount / ((int)coin / 100M));
+        }
+    }
+
+    internal static bool ChangeDispensedReducesAmountDepositedToZero((int quarters, int dimes, int nickels) change, decimal amountDeposited)
+    {
+        decimal quarterVal = (int)Coins.Quarter * change.quarters / 100;
+        decimal dimeVal = (int)Coins.Dime * change.dimes / 100;
+        decimal nickelVal = (int)Coins.Nickel * change.nickels / 100;
+        decimal totalChange = quarterVal + dimeVal + nickelVal;
+        
+        return totalChange == amountDeposited;
+    }
+}
+internal enum Coins
+{
+    Quarter = 25,
+    Dime = 10,
+    Nickel = 5,
+}
+
+
