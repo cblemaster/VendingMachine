9f56eabd5b69458d7f4a9295e92a4f2ddf5a2892
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-30 15:42:16 -0400

    add model

---
+++ VendingMachine/Inventory/Inventory.cs
@@ -0,0 +1,10 @@
+﻿
+namespace VendingMachine.Inventory;
+
+internal sealed class Inventory
+{
+    private Dictionary<string, IEnumerable<Snack>> _snacks = [];
+    private string[] _locations = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"];
+
+    internal void FillInventory() { }
+}
