a53a252a41c4e78f47932a15e1ea7f5f8e968707
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-30 15:43:33 -0400

    update model

--- VendingMachine/Owner/Owner.cs
+++ VendingMachine/Owner/Owner.cs
@@ -1,11 +1,7 @@
-﻿using VendingMachine.Inventory;
-using VendingMachine.Machine;
-
+﻿
 namespace VendingMachine.Owner;
 
-internal static class Owner
+internal sealed class Owner
 {
-    internal static void TurnVendingMachineOn(Vendomatic vm) => vm.IsOn = true;
-    internal static void TurnVendingMachineOff(Vendomatic vm) => vm.IsOn = false;
-    internal static IEnumerable<Slot> UpdateVendingMachineInventory(Vendomatic vm) => ProcessInventory.CreateVendingMachineInventory(vm);
+    
 }
