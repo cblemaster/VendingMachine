53185897c26d4e72ddc48d9b1534f9dc3cb72c59
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-30 15:41:49 -0400

    update model

--- VendingMachine/Customer/Customer.cs
+++ VendingMachine/Customer/Customer.cs
@@ -1,16 +1,7 @@
-﻿using VendingMachine.Machine;
-using VendingMachine.Reporting;
-
+﻿
 namespace VendingMachine.Customer;
 
-internal static class Customer
+internal sealed class Customer
 {
-    internal static void DepositMoney(Vendomatic vm, decimal amount)
-    {
-        if (amount <= 0) { return; }
-        vm.AmountDeposited += amount;
-        vm.AuditFile.AppendLine(AuditFileLineItem.MoneyDeposited(amount, vm.AmountDeposited));
-    }
-
-    internal static string SelectSnack(Vendomatic vm, Slot slot) => vm.DispenseSnack(slot);
+    
 }
