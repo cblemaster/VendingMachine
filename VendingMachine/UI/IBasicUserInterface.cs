e80489540bf20a462daca122c927523351560da5
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-30 15:46:53 -0400

    delete unneeded files

--- VendingMachine/UI/IBasicUserInterface.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VendingMachine.UI;
-
-public interface IBasicUserInterface
-{
-    void Output(string message);
-    void PauseOutput();
-    object PromptForSelection(object[] options);
-}
