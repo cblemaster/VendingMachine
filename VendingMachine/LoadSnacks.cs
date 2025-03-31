91540e5376668c4e86f8c2611ead2cf7e84827bb
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-31 19:53:38 -0400

    add load snack

---
+++ VendingMachine/LoadSnacks.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.Json;
+using VendingMachine.Snacks;
+
+namespace VendingMachine;
+
+internal sealed class LoadSnacks
+{
+    internal string ReadInventoryToString()
+    {
+        string currentDirectory = Environment.CurrentDirectory;
+        string fullInventoryPath = Path.Combine(currentDirectory, @"..\..\..\", "inventory.json");  // TODO: magic string
+
+        using StreamReader sr = new(fullInventoryPath);
+        return sr.ReadToEnd();
+    }
+
+    internal IEnumerable<SnackJsonModel> DeserializeInventoryString(string json)
+    {
+        JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
+        return JsonSerializer.Deserialize<IEnumerable<SnackJsonModel>>(json, options) ?? [];
+    }
+}
