8ed8e6903f972e286f10dd0c9f27d7681ac718f8
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-30 22:18:33 -0400

    add classes centered around behavior

---
+++ VendingMachine/Coin.cs
@@ -0,0 +1,17 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class Coin
+{
+    internal decimal Value { get; }
+    internal int Count { get; }
+    internal static string Singular => "quarter";
+    internal static string Plural => $"{Singular}s";
+    internal decimal ValueSum => Value * Count;
+
+    internal Coin(decimal amount, decimal coinValue)
+    {
+        Value = coinValue;
+        Count = (int)(amount / coinValue);
+    }
+}
---
+++ VendingMachine/DepositFunds.cs
@@ -0,0 +1,17 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class DepositFunds
+{
+    private decimal Amount { get; }
+    private DateTimeOffset TimeStamp { get; }
+
+    internal DepositFunds(decimal amount)
+    {
+        Amount = amount;
+        TimeStamp = DateTimeOffset.UtcNow;
+    }
+
+    internal new string ToString() => $"{Amount:C} Deposited";
+    internal string ToReportString => $"{TimeStamp:f} - ${Amount:C} Deposited";
+}
