d0e741e1fdc56badd71aac62f8a73b93d3644ba6
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-31 18:00:23 -0400

    add slot model

---
+++ VendingMachine/Machine/Slot.cs
@@ -0,0 +1,12 @@
+﻿
+using VendingMachine.Snacks;
+
+namespace VendingMachine.Machine;
+
+internal class Slot
+{
+    private const int SLOT_CAPACITY = 5;
+
+    internal string Identifier { get; }
+    internal Snack[] Snacks { get; }
+}
