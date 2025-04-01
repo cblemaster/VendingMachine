c7f159df47e243869abd09a3dc363eb32475093f
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 19:49:45 -0400

    add snack json model

---
+++ VendingMachine/SnackJsonModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class SnackJsonModel
+{
+    internal string Type { get; init; } = string.Empty;
+    internal decimal Price { get; init; }
+    internal string Label { get; init; } = string.Empty;
+}
