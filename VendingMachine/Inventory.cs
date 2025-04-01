b7cfe500dee258f3c447bfaa31ca4b86cc4057c0
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 18:35:18 -0400

    Add inventory and snack slot models

---
+++ VendingMachine/Inventory.cs
@@ -0,0 +1,14 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class Inventory
+{
+    private SnackSlot[] _snackSlots;
+
+    internal SnackSlot[] SnackSlots => _snackSlots;
+
+    internal Inventory(SnackSlot[] snackSlots)
+    {
+        _snackSlots = snackSlots;
+    }
+}
---
+++ VendingMachine/SnackSlot.cs
@@ -0,0 +1,22 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class SnackSlot
+{
+    private List<Snack> _snacks;
+    
+    internal string Identifier { get; }
+    internal List<Snack> Snacks => _snacks;
+
+    internal SnackSlot(string identifier)
+    {
+        Identifier = identifier;
+        _snacks = [];
+    }
+
+    internal void AddSnack(Snack snack) => _snacks.Add(snack);
+
+    internal void AddSnacks(Snack[] snacks) => _snacks.AddRange(snacks);
+
+    internal void RemoveSnack(Snack snack) => _snacks.Remove(snack);
+}
