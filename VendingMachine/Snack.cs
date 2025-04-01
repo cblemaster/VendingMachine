8da0fef85670ad1532ed5c63028a06fda1ff3340
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 18:08:55 -0400

    add snack model

---
+++ VendingMachine/Snack.cs
@@ -0,0 +1,16 @@
+﻿
+namespace VendingMachine;
+
+internal class Snack
+{
+    internal decimal Price { get; }
+    internal string Label { get; }
+
+    internal Snack(decimal price, string label)
+    {
+        Price = price;
+        Label = label;
+    }
+
+    internal virtual void SomethingPolymorphic() { }
+}
