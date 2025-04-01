c937ac24301f02c4dbbe22970d0ee4004a8c5776
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 18:42:52 -0400

    add vendtron model

---
+++ VendingMachine/VendTron.cs
@@ -0,0 +1,18 @@
+﻿
+namespace VendingMachine;
+
+internal sealed class VendTron
+{
+    private Snack[] _purchases = [];
+    private decimal[] _deposits = [];
+    private decimal[] _changeReturned = [];
+    
+    internal Inventory Inventory { get; }
+    internal decimal Deposits { get; private set; }
+
+    internal VendTron()
+    {
+        Inventory = default!;
+        // LOAD INVENTORY!
+    }
+}
