c1a37e1302deddfb1dba62e38fbeb5cca9d65e5a
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 21:57:45 -0400

    add methods to vendtron model

--- VendingMachine/VendTron.cs
+++ VendingMachine/VendTron.cs
@@ -3,8 +3,12 @@ namespace VendingMachine;
 
 internal sealed class VendTron
 {
-    private List<Snack> _purchases;
+    private const decimal QUARTER = 0.25m;
+    private const decimal DIME = 0.10m;
+    private const decimal NICKEL = 0.05m;
+
     private List<decimal> _deposits;
+    private List<Snack> _purchases;
     private List<decimal> _changeReturned;
     
     internal Inventory Inventory { get; }
@@ -18,24 +22,71 @@ internal sealed class VendTron
         _changeReturned = [];
     }
 
-    internal void DisplaySnacks()
-    {
-
-    }
+    internal string DisplaySnacks() => Inventory.DisplaySnacks();
     
     internal void ProcessDeposit(decimal deposit)
     {
-        _deposits.Add(deposit);
-        // TODO: add deposit to reports
+        if (deposit <= 0)
+        {
+            throw new ArgumentException("Zero deposit not allowed...");
+        }
+        else if (Inventory.SnackSlots.All(s => s.Snacks.Count == 0))
+        {
+            throw new InvalidOperationException("Cannot deposit when there are no snacks to purchase...");
+        }
+        else
+        {
+            Deposits += deposit;
+            _deposits.Add(deposit);
+        }
     }
 
     internal void ProcessPurchase(string identifier)
     {
-
+        if (Inventory.SnackSlots.SingleOrDefault(s => s.Identifier == identifier) is SnackSlot snackSlot)
+        {
+            if (snackSlot.Snacks.Count == 0)
+            {
+                throw new InvalidOperationException("Snack is sold out, and cannot be purchased...");
+            }
+            else if (snackSlot.Snacks.First().Price > Deposits)
+            {
+                throw new ArgumentException("Insufficient deposits for snack purchase...");
+            }
+            else
+            {
+                Snack snack = snackSlot.Snacks.First();
+                
+                snackSlot.RemoveSnack(snack);
+                _purchases.Add(snack);
+            }
+        }
     }
 
-    internal void ProcessReturnChange()
+    internal string ProcessReturnChange()
     {
+        if (Deposits == 0)
+        {
+            throw new InvalidOperationException("There is no change due...");
+        }
+        
+        decimal changeReturned = Deposits;
 
+        int quarters = (int)(Deposits / QUARTER);
+        Deposits -= quarters * QUARTER;
+        int dimes = (int)(Deposits / DIME);
+        Deposits -= dimes * DIME;
+        int nickels = (int)(Deposits / NICKEL);
+        Deposits -= nickels * NICKEL;
+
+        if (Deposits != 0)
+        {
+            throw new ArithmeticException("Error calculating change...");
+        }
+        else
+        {
+            _changeReturned.Add(changeReturned);
+            return $"Your change is {quarters} quarter(s), {dimes} dime(s), and {nickels} nickels...";
+        }
     }
 }
