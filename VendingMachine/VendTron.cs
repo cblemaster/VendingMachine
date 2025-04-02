374100eaf8e0eecfc482211113f824a299e3b17f
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-01 20:31:48 -0400

    update vendtron model

--- VendingMachine/VendTron.cs
+++ VendingMachine/VendTron.cs
@@ -3,16 +3,39 @@ namespace VendingMachine;
 
 internal sealed class VendTron
 {
-    private Snack[] _purchases = [];
-    private decimal[] _deposits = [];
-    private decimal[] _changeReturned = [];
+    private List<Snack> _purchases;
+    private List<decimal> _deposits;
+    private List<decimal> _changeReturned;
     
     internal Inventory Inventory { get; }
     internal decimal Deposits { get; private set; }
 
     internal VendTron()
     {
-        Inventory = default!;
-        // LOAD INVENTORY!
+        Inventory = new();
+        _purchases = [];
+        _deposits = [];
+        _changeReturned = [];
+    }
+
+    internal void DisplaySnacks()
+    {
+
+    }
+    
+    internal void ProcessDeposit(decimal deposit)
+    {
+        _deposits.Add(deposit);
+        // TODO: add deposit to reports
+    }
+
+    internal void ProcessPurchase(string identifier)
+    {
+
+    }
+
+    internal void ProcessReturnChange()
+    {
+
     }
 }
