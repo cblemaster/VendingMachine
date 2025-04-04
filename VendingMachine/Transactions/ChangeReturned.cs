1f14224c11f60d18122e0716d28f0d053c246078
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-04 15:52:39 -0400

    update change return algorithm

--- VendingMachine/Transactions/ChangeReturned.cs
+++ VendingMachine/Transactions/ChangeReturned.cs
@@ -21,18 +21,21 @@ internal sealed class ChangeReturned : Transaction
 
     private void CalculateChangeOrThrow()
     {
+        // TODO: is there change to return?
+
         decimal changeToReturn = ChangeAmount;
 
-        _countOfQuarters = (int)(ChangeAmount / QUARTER);
-        changeToReturn -= _countOfQuarters * QUARTER;
-        _countOfDimes = (int)(ChangeAmount / DIME);
-        changeToReturn -= _countOfDimes * DIME;
-        _countOfNickels = (int)ChangeAmount;
+        _countOfNickels = (int)(changeToReturn / NICKEL);
+        _countOfQuarters = _countOfNickels / (int)(QUARTER / NICKEL);
+        _countOfNickels -= _countOfQuarters * (int)(QUARTER / NICKEL);
+        _countOfDimes = _countOfNickels / (int)(DIME / NICKEL);
+        _countOfNickels -= _countOfDimes * (int)(DIME / NICKEL);
 
-        if (changeToReturn != 0)
-        {
-            throw new ArithmeticException("Error calculating change...");
-        }
+        // TODO...
+        //if (changeToReturn != 0)
+        //{
+        //    throw new ArithmeticException("Error calculating change...");
+        //}
     }
 
     internal override string ToDisplayString => $"Your change is: {_countOfQuarters} quarter(s), {_countOfDimes} dime(s), and {_countOfNickels} nickel(s)...";
