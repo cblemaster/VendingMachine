f450120c209f8c7593d2ac67286d52f4ebcec650
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-04 16:03:33 -0400

    code cleanup

--- VendingMachine/Reports/SalesReport.cs
+++ VendingMachine/Reports/SalesReport.cs
@@ -7,16 +7,16 @@ namespace VendingMachine.Reports;
 internal sealed class SalesReport(IEnumerable<Transaction> transactions)
 {
     private readonly IEnumerable<Transaction> _transactions = transactions;
-    private List<SnackPurchased> _snacksPurchased => _transactions.OfType<SnackPurchased>().ToList();
+    private List<SnackPurchased> SnacksPurchased => [.. _transactions.OfType<SnackPurchased>()];
 
     internal string GenerateReport()
     {
         StringBuilder sb = new();
         sb = sb.AppendLine($"SALES REPORT FOR {DateTimeOffset.UtcNow.ToString("F")}\n");
 
-        foreach (SnackPurchased snack in _snacksPurchased)
+        foreach (SnackPurchased snack in SnacksPurchased)
         {
-            int countOfSnackPurchased = _snacksPurchased.Count(s => s.Snack.Label == snack.Snack.Label);
+            int countOfSnackPurchased = SnacksPurchased.Count(s => s.Snack.Label == snack.Snack.Label);
             decimal price = snack.Snack.Price;
             decimal extended = countOfSnackPurchased * price;
             sb = sb.AppendLine(snack.ToReportString);
--- VendingMachine/Reports/SummaryReport.cs
+++ VendingMachine/Reports/SummaryReport.cs
@@ -6,7 +6,7 @@ namespace VendingMachine.Reports;
 
 internal sealed class SummaryReport(IEnumerable<Transaction> snacksSold)
 {
-    private readonly List<Transaction> _snacksSold = snacksSold.ToList();
+    private readonly List<Transaction> _snacksSold = [.. snacksSold];
 
     internal string GenerateReport()
     {
